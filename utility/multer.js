7c21042af854a0860b79f3a912fb3db4c485e734
aniket44788 <darkpanda44788@gmail.com>
2025-05-23 07:44:56 +0530

    hkjfkd

--- utility/multer.js
+++ utility/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer")
 const path = require("path")
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './upload/')
+    cb(null, './upload')
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
